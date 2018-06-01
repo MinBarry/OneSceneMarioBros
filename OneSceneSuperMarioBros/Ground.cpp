fc3d531ab701514cf2ac7d186e4358a56573f7af
MinBarry <minna.barry@gmail.com>
2018-06-01 15:18:45 -0700

    Fix collision with ground

--- OneSceneSuperMarioBros/Ground.cpp
+++ OneSceneSuperMarioBros/Ground.cpp
@@ -18,31 +18,26 @@ Ground::~Ground()
 
 void Ground::_onCollision(GameObject& other)
 {
-	//if bottoms intersect 
 	sf::FloatRect otherbounds = other.sprite.getGlobalBounds();
 	sf::FloatRect bounds = sprite.getGlobalBounds();
-	if(otherbounds.top <= bounds.top && 
+	if(otherbounds.top+otherbounds.height >= bounds.top && otherbounds.top+otherbounds.height-20 < bounds.top &&
 		(otherbounds.left > bounds.left || otherbounds.left+otherbounds.width > bounds.left)&& 
 		(otherbounds.left < bounds.left+bounds.width || otherbounds.left+otherbounds.width < bounds.left)){
 		other.sprite.setPosition(sf::Vector2f(other.sprite.getPosition().x, sprite.getPosition().y - other.sprite.getGlobalBounds().height));
 		if (typeid(other) == typeid(Player)) {
-			std::cout << "Player is stepping on me!";
 			Player & player = (Player &)other;
 			player.setIsGrounded();
 			playSound(playerSteps);
 		}
 	}
-	if (otherbounds.top >= bounds.top &&
+	if (otherbounds.top >= bounds.top && otherbounds.top+otherbounds.height >= bounds.top &&
 		(otherbounds.left < bounds.left && otherbounds.left + otherbounds.width > bounds.left)) {
 		other.sprite.setPosition(sf::Vector2f(bounds.left-otherbounds.width, other.sprite.getPosition().y));
 	}
-	if (otherbounds.top >= bounds.top &&
+	if (otherbounds.top >= bounds.top && otherbounds.top + otherbounds.height >= bounds.top &&
 		(otherbounds.left < bounds.left+bounds.width && otherbounds.left + otherbounds.width > bounds.left+bounds.width)) {
 		other.sprite.setPosition(sf::Vector2f(bounds.left+bounds.width, other.sprite.getPosition().y));
 	}
 }
 
-void Ground::update()
-{
-}
 
